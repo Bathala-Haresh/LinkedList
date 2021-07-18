2f2477018b155bcc95f531c2b758f4dadb5e6113
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-19 00:08:29 +0530

    added:program to add elements in linkedlist

--- LinkedList.java
+++ LinkedList.java
@@ -1,57 +1,28 @@
 package com.demo;
-
-public class LinkedListUC1{    
-	//Represent a node of the singly linked list    
-	class Node{    
-		int data;    
-		Node next;    
-
-		public Node(int data) {    
-			this.data = data;    
-			this.next = null;    
-		}    
-	}    
-	
-
-	//Represent the head and tail of the singly linked list    
-	public Node head = null;    
-	public Node tail = null;    
-
-	//addNode() will add a new node to the list    
-	public void addNode(int data) {    
-		//Create a new node    
-		Node newNode = new Node(data);    
-
-		//Checks if the list is empty    
-		if(head == null) {    
-			//If list is empty, both head and tail will point to new node    
-			head = newNode;    
-			tail = newNode;    
-		}    
-		else {    
-			//newNode will be added after tail such that tail's next will point to newNode    
-			tail.next = newNode;    
-			//newNode will become new tail of the list    
-			tail = newNode;    
-		}    
-	}    
-
-	//display() will display all the nodes present in the list    
-	public void display() {    
-		//Node current will point to head    
-		Node current = head;    
-
-		if(head == null) {    
-			System.out.println("List is empty");    
-			return;    
-		}    
-		System.out.println("Nodes of singly linked list: ");    
-		while(current != null) {    
-			//Prints each node by incrementing pointer    
-			System.out.print(current.data + " ");    
-			current = current.next;    
-		}    
-		System.out.println();    
-	}    
+import java.util.Scanner;
+//main class
+public class Linkedlist
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		Node List=null;
+		Node temp;
+		while(true)
+		{
+			System.out.println("1.Add\2.Exit");
+			int num;
+			System.out.print("Your Choice:");
+			int choice=sc.nextInt();
+			switch(choice)
+			{
+			case 1:List=List.add(List);
+			break;
+			case 2:System.exit(0);
+			break;
+			default:System.out.println("You Entered Wrong Value");
+			break;
+			}
+		}
+	}
 
 }
--- Node.java
+++ Node.java
@@ -1,16 +1,34 @@
+package com.demo;
+import java.util.Scanner;
 
-public class MyLinkedList {
-
-	public static void main(String[] args) {   
-
-		LinkedListUC1 sList = new LinkedListUC1();    
-
-		//Add nodes to the list    
-		sList.addNode(56);    
-		sList.addNode(30);    
-		sList.addNode(70);      
-
-		//Displays the nodes present in the list    
-		sList.display();    
-	}  
-}
+class Node
+{
+	int data;
+	Node next;
+	public Node(int data)
+	{
+		this.data=data;
+	}
+	//method to add elements in linkedlist
+	public static Node add(Node List)
+	{
+		int num;
+		Node temp;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number you want to insert:");
+		num=sc.nextInt();
+		//if list is null then adding new node in the list
+		if(List==null)
+		{
+			List=new Node(num);
+		}
+		//if list is not null then storing the new node in temp and shifting its temp element to first position in list
+		else if(List!=null)
+		{
+			temp=new Node(num);
+			temp.next=List;
+			List=temp;
+		}
+		Node.display(List);
+		return List;
+	}
